618e587106f5b28efbd51efe3947382416861ac2
unknown <carlos.riquelme@gmail.com>
2019-03-27 17:28:50 -0300

    Aplicadas las políticas de seguridad

---
+++ assets/js/transition.js
@@ -0,0 +1,11 @@
+$("#example").vegas({
+    timer: false,
+    delay: 6000,
+    transitionDuration: 2000,
+    transition: "blur",
+    slides: [
+        { src: "assets/img/slide1.jpg" },
+        { src: "assets/img/slide2.jpg" },
+        { src: "assets/img/slide3.jpg" }
+    ]
+});
